ed0c3220abe1b8688f144409c854a203c8a28466
tmazyrko <tmazyrko@gmail.com>
2021-04-05 03:13:35 -0400

    fixed function export statement

--- src/js/generateTable.js
+++ src/js/generateTable.js
@@ -1,4 +1,4 @@
-function generateTable(table, data) {
+export default function generateTable(table, data) {
     for (let element of data) {
         let row = table.insertRow();
         console.log(element);
