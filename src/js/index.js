a3404a2c7213af58d335f18213a6878eccfe9155
tmazyrko <tmazyrko@gmail.com>
2021-04-19 14:59:38 -0400

    changed indents from 4 spaces to 2 spaces

--- src/js/index.js
+++ src/js/index.js
@@ -2,19 +2,19 @@ import getJSON from "./getJSON";
 import TableGen from "./TableGen";
 
 getJSON("", function (data) {
-    console.log(data);
+  console.log(data);
 });
 
 getJSON('http://localhost:8000/api/v1/cities',
-    function(err, records) {
-        if (err !== null) {
-            alert('Something went wrong: ' + err);
-        } else {
-            let table = document.querySelector("table");
-            let data = Object.keys((records.data[0]));
-            let dataRecords = records.data;
+  function (err, records) {
+    if (err !== null) {
+      alert('Something went wrong: ' + err);
+    } else {
+      let table = document.querySelector("table");
+      let data = Object.keys((records.data[0]));
+      let dataRecords = records.data;
 
-            new TableGen(table, data, dataRecords);
-        }
+      new TableGen(table, data, dataRecords);
+    }
 
-    });
+  });
