beb2314c5fbab76a928b2487c4c5cc1c18877e7f
tmazyrko <tmazyrko@gmail.com>
2021-04-19 17:12:42 -0400

    removed unused port constant

--- server/app.js
+++ server/app.js
@@ -3,14 +3,10 @@ const open = require('open');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+// create express app
 const app = express();
 app.use(cors());
 app.use(express.static('docs'));
-
-// create express app
-
-// Setup server port
-const port = process.env.PORT || 5000;
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
