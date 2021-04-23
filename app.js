b35fd3359e597cd0bdc5ac4d4bb680955a69d835
tmazyrko <tmazyrko@gmail.com>
2021-04-22 23:06:00 -0400

    implement login form handler

--- app.js
+++ app.js
@@ -94,3 +94,33 @@ app.get('/login', (req, res) => {
 const generateAuthToken = () => {
   return crypto.randomBytes(30).toString('hex');
 }
+
+// This will hold the users and authToken related to users
+const authTokens = {};
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const hashedPassword = getHashedPassword(password);
+
+  const user = users.find(u => {
+    return u.email === email && hashedPassword === u.password
+  });
+
+  if (user) {
+    const authToken = generateAuthToken();
+
+    // Store authentication token
+    authTokens[authToken] = user;
+
+    // Setting the auth token in cookies
+    res.cookie('AuthToken', authToken);
+
+    // Redirect user to the protected page
+    res.redirect('/protected');
+  } else {
+    res.render('login', {
+      message: 'Invalid username or password',
+      messageClass: 'alert-danger'
+    });
+  }
+});
