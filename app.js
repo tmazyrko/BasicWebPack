1face2d21abc95cb985928fb8f5a6fafaa57b4a4
tmazyrko <tmazyrko@gmail.com>
2021-04-22 22:47:38 -0400

    init/configure Express app and Handlebars

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// To support URL-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// To parse cookies from the HTTP Request
+app.use(cookieParser());
+
+app.engine('hbs', exphbs({
+  extname: '.hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+// Our requests handlers will be implemented here...
+
+app.listen(3000);
