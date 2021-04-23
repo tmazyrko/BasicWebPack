92eb6d9446e8a01ed2e287eb0bdcd30e4a38b4e6
tmazyrko <tmazyrko@gmail.com>
2021-04-22 23:00:09 -0400

    add array for storing registered users

--- app.js
+++ app.js
@@ -36,3 +36,14 @@ const getHashedPassword = (password) => {
   const hash = sha256.update(password).digest('base64');
   return hash;
 }
+
+const users = [
+  // This user is added to the array to avoid creating a new user on each restart
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'johndoe@email.com',
+    // This is the SHA256 hash for value of `password`
+    password: 'XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg='
+  }
+];
