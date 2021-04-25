3a2824b66435dee4cf35becabf84d8b56d340376
tmazyrko <tmazyrko@gmail.com>
2021-04-24 22:35:59 -0400

    moved Basic Auth app.js into server folder

--- server/app.js
+++ server/app.js
@@ -1,37 +1,151 @@
-'use strict';
 const express = require('express');
-const open = require("open");
+const exphbs = require('express-handlebars');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-var cors = require('cors')
+
 const app = express();
-app.use(cors())
-app.use(express.static('docs'));
 
-// create express app
+// To support URL-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// To parse cookies from the HTTP Request
+app.use(cookieParser());
 
-// Setup server port
-const port = process.env.PORT || 5000;
+app.engine('hbs', exphbs({
+  extname: '.hbs'
+}));
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'hbs');
 
-// parse requests of content-type - application/json
-app.use(bodyParser.json())
+// Our requests handlers will be implemented here...
 
-// define a root route
+app.listen(3000);
 
-// Require employee routes
-const citiesRoutes = require('./routes/cities.routes')
+app.get('/', function (req, res) {
+  res.render('home');
+});
 
-// using as middleware
-app.use('/api/v1/cities', citiesRoutes)
+app.get('/register', (req, res) => {
+  res.render('register');
+});
 
-app.set('port', process.env.PORT || 8000);
-app.set('ip', process.env.NODEJS_IP || '127.0.0.1');
+const crypto = require('crypto');
 
-app.listen(app.get('port'), function() {
-    console.log('%s: Node server started on %s ...', Date(Date.now()), app.get('port'));
-    open("http://localhost:8000");
+const getHashedPassword = (password) => {
+  const sha256 = crypto.createHash('sha256');
+  const hash = sha256.update(password).digest('base64');
+  return hash;
+}
 
+const users = [
+  // This user is added to the array to avoid creating a new user on each restart
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'johndoe@email.com',
+    // This is the SHA256 hash for value of `password`
+    password: 'XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg='
+  }
+];
+
+app.post('/register', (req, res) => {
+  const { email, firstName, lastName, password, confirmPassword } = req.body;
+
+  // Check if the password and confirm password fields match
+  if (password === confirmPassword) {
+
+    // Check if user with the same email is also registered
+    if (users.find(user => user.email === email)) {
+
+      res.render('register', {
+        message: 'User already registered.',
+        messageClass: 'alert-danger'
+      });
+
+      return;
+    }
+
+    const hashedPassword = getHashedPassword(password);
+
+    // Store user into the database if you are using one
+    users.push({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword
+    });
+
+    res.render('login', {
+      message: 'Registration Complete. Please login to continue.',
+      messageClass: 'alert-success'
+    });
+  } else {
+    res.render('register', {
+      message: 'Password does not match.',
+      messageClass: 'alert-danger'
+    });
+  }
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+const generateAuthToken = () => {
+  return crypto.randomBytes(30).toString('hex');
+}
+
+// This will hold the users and authToken related to users
+const authTokens = {};
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const hashedPassword = getHashedPassword(password);
+
+  const user = users.find(u => {
+    return u.email === email && hashedPassword === u.password
+  });
+
+  if (user) {
+    const authToken = generateAuthToken();
+
+    // Store authentication token
+    authTokens[authToken] = user;
+
+    // Setting the auth token in cookies
+    res.cookie('AuthToken', authToken);
+
+    // Redirect user to the protected page
+    res.redirect('/protected');
+  } else {
+    res.render('login', {
+      message: 'Invalid username or password',
+      messageClass: 'alert-danger'
+    });
+  }
+});
+
+app.use((req, res, next) => {
+  // Get auth token from the cookies
+  const authToken = req.cookies['AuthToken'];
+
+  // Inject the user to the request
+  req.user = authTokens[authToken];
+
+  next();
+});
+
+const requireAuth = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.render('login', {
+      message: 'Please login to continue',
+      messageClass: 'alert-danger'
+    });
+  }
+};
+
+app.get('/protected', requireAuth, (req, res) => {
+  res.render('protected');
 });
