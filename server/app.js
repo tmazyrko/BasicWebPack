592cf303faf74001228ae7957aacc434f17c8a8d
tmazyrko <tmazyrko@gmail.com>
2021-04-24 23:16:12 -0400

    implement Auth0 Strategy definition

--- server/app.js
+++ server/app.js
@@ -30,7 +30,21 @@ if(app.get('env') === 'production') {
 }
 
 // Passport Configuration
-
+const strategy = new Auth0Strategy(
+  {
+      domain: process.env.AUTH0_DOMAIN,
+      clientID: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      callbackURL: process.env.AUTH0_CALLBACK_URL
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+      // Access tokens are used to authorize users to an API
+      // accessToken is the token to call the Auth0 API or a secured 3rd-party API
+      // extraParams.id_token has the JSON Web Token (JWT)
+      // profile has all the information from the user
+      return done(null, profile);
+  }
+);
 
 // App Configuration
 app.set('views', path.join(__dirname, 'views'));
