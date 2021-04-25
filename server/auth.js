c8e3ea293ad52598f5eebaf22a8a5ead626553c7
tmazyrko <tmazyrko@gmail.com>
2021-04-24 23:23:01 -0400

    fix code sectioning

--- server/auth.js
+++ server/auth.js
@@ -1,10 +1,10 @@
-
-
-// Routes Definitions
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const querystring = require('querystring');
+
+// Routes Definitions
+
 
 // Module Exports
 
