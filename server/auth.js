aff834ae1d2c02168e0ed6c3b8d152e2bcc330a5
tmazyrko <tmazyrko@gmail.com>
2021-04-24 23:22:06 -0400

    import required packages

--- server/auth.js
+++ server/auth.js
@@ -1,7 +1,10 @@
 
 
 // Routes Definitions
-
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const querystring = require('querystring');
 
 // Module Exports
 
