4db1167cf98dfbc705d6a4b083354ec28a051a80
tmazyrko <tmazyrko@gmail.com>
2021-04-24 23:29:00 -0400

    create 'GET /callback' endpoint

--- server/auth.js
+++ server/auth.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 const querystring = require('querystring');
 
 // Routes Definitions
+
 router.get(
   '/login',
   passport.authenticate('auth0', {
@@ -13,6 +14,25 @@ router.get(
     res.redirect('/');
   }
 );
+
+router.get('/callback', (req, res, next) => {
+  passport.authenticate('auth0', (err, user, info) => {
+    if(err){
+      return next(err);
+    }
+    if(!user){
+      return res.redirect('/login');
+    }
+    req.logIn(user, (err) => {
+      if(err){
+        return next(err);
+      }
+      const returnTo = req.session.returnTo;
+      delete req.session.returnTo;
+      res.redirect(returnTo || '/');
+    });
+  })(req, res, next);
+});
 
 // Module Exports
 
