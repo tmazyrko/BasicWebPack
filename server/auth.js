befb890676daba331514f8b5d21274aae7ef4edc
tmazyrko <tmazyrko@gmail.com>
2021-04-24 23:24:52 -0400

    create 'GET /login' endpoint

--- server/auth.js
+++ server/auth.js
@@ -4,7 +4,15 @@ const passport = require('passport');
 const querystring = require('querystring');
 
 // Routes Definitions
-
+router.get(
+  '/login',
+  passport.authenticate('auth0', {
+    scope: 'openid email profile'
+  }),
+  (req, res) => {
+    res.redirect('/');
+  }
+);
 
 // Module Exports
 
