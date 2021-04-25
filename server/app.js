2eeea32fe626d6d55513395b7958a02686e1b54f
tmazyrko <tmazyrko@gmail.com>
2021-04-24 23:12:20 -0400

    configured session middleware

--- server/app.js
+++ server/app.js
@@ -33,7 +33,11 @@ if(app.get('env') === 'production') {
 
 
 // App Configuration
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(expressSession(session));
 
 /*
 // create express app
