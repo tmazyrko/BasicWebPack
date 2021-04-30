8222f9327776f58235bf098f4bdfededf0959831
tmazyrko <tmazyrko@gmail.com>
2021-04-30 00:29:53 -0400

    set up express app to use JSON body parser

--- server/auth.js
+++ server/auth.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.listen(3000, () => {
   console.log('Authentication service started on port 3000');
