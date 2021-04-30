04662cab0518b38cef8476d89a0437409e15fab8
tmazyrko <tmazyrko@gmail.com>
2021-04-30 01:25:31 -0400

    add request handler for adding new book

--- server/app.js
+++ server/app.js
@@ -58,3 +58,16 @@ const authenticateJWT = (req, res, next) => {
 app.get('/books', authenticateJWT, (req, res) => {
     res.json(books);
 });
+
+app.post('/books', authenticateJWT, (req, res) => {
+    const { role } = req.user;
+
+    if(role !== 'admin'){
+        return res.sendStatus(403);
+    }
+
+    const book = req.body;
+    books.push(book);
+
+    res.send('Book added successfully');
+});
