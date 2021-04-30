b1916df3d25009bec2a4ecdd44f20bd4bbadd196
tmazyrko <tmazyrko@gmail.com>
2021-04-30 01:06:39 -0400

    create user login request handler

--- server/auth.js
+++ server/auth.js
@@ -22,3 +22,22 @@ const users = [
 ];
 
 const accessTokenSecret = 'testSecret123';
+
+app.post('/login', (req, res) => {
+  // Read username and password from request body
+  const { username, password } = req.body;
+
+  // Filter user from the users array by username and password
+  const user = users.find(u => { return u.username === username && u.password === password });
+
+  if(user){
+      // Generate an access token
+      const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret);
+
+      res.json({
+        accessToken
+      });
+  } else {
+      res.send('Username or password incorrect');
+  }
+});
