d1b1bb0ced27efd983b9f62c519157981166a58f
tmazyrko <tmazyrko@gmail.com>
2021-04-30 01:13:32 -0400

    create array to simulate a database

--- server/app.js
+++ server/app.js
@@ -8,3 +8,30 @@ app.use(bodyParser.json());
 app.listen(4000, () => {
     console.log('Authentication service started on port 4000');
 });
+
+const books = [
+    {
+        "author": "Chinua Achebe",
+        "country": "Nigeria",
+        "language": "English",
+        "pages": 209,
+        "title": "Things Fall Apart",
+        "year": 1958
+    },
+    {
+        "author": "Hans Christian Andersen",
+        "country": "Denmark",
+        "language": "Danish",
+        "pages": 784,
+        "title": "Fairy tales",
+        "year": 1836
+    },
+    {
+        "author": "Dante Alighieri",
+        "country": "Italy",
+        "language": "Italian",
+        "pages": 928,
+        "title": "The Divine Comedy",
+        "year": 1315
+    },
+]
