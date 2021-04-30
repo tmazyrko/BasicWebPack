ad25b247939cebd591c20c3dde083dded06566f1
tmazyrko <tmazyrko@gmail.com>
2021-04-30 00:27:20 -0400

    created array (for poc) to hold users

--- server/auth.js
+++ server/auth.js
@@ -4,3 +4,15 @@ const app = express();
 app.listen(3000, () => {
   console.log('Authentication service started on port 3000');
 });
+
+const users = [
+  {
+    username: 'john',
+    password: 'password123admin',
+    role: 'admin'
+  },{
+    username: 'anna',
+    password: 'password123member',
+    role: 'member'
+  }
+];
