5ffdff57ff44175e648c4c6053e1c74d5e68b3e0
tmazyrko <tmazyrko@gmail.com>
2021-05-10 13:46:10 -0400

    move JWT authentication into cities router

--- server/app.js
+++ server/app.js
@@ -78,26 +78,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// Authorization middleware. When used, the
-// Access Token must exist and be verified against
-// the Auth0 JSON Web Key Set
-const checkJwt = jwt({
-  // Dynamically provide a signing key
-  // based on the kid in the header and
-  // the signing keys provided by the JWKS endpoint.
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://tmaz-dev.us.auth0.com/.well-known/jwks.json`
-  }),
-
-  // Validate the audience and the issuer.
-  audience: 'http://localhost:3000/api/v1/cities',
-  issuer: [`https://tmaz-dev.us.auth0.com/`],
-  algorithms: ['RS256']
-});
-
 // Router Mounting
 app.use('/', authRouter);
 app.use('/api/private', citiesRoutes)
@@ -131,11 +111,11 @@ app.get('/api/public', function(req, res) {
 });
 
 // This route needs authentication
-app.get('/api/private', checkJwt, function(req, res) {
+/*app.get('/api/private', checkJwt, function(req, res) {
   res.json({
     message: 'Hello from a private endpoint! You need to be authenticated to see this.'
   });
-});
+});*/
 
 // Server Activation
 app.listen(port, () => {
--- server/routes/cities.routes.js
+++ server/routes/cities.routes.js
@@ -2,19 +2,43 @@ const express = require('express')
 const router = express.Router()
 const cityController = require('../controllers/cities.controllers');
 
+const jwt = require('express-jwt');
+const jwtAuthz = require('express-jwt-authz');
+const jwksRsa = require('jwks-rsa');
+
+// Authorization middleware. When used, the
+// Access Token must exist and be verified against
+// the Auth0 JSON Web Key Set
+const checkJwt = jwt({
+  // Dynamically provide a signing key
+  // based on the kid in the header and
+  // the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://tmaz-dev.us.auth0.com/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: 'http://localhost:3000/api/v1/cities',
+  issuer: [`https://tmaz-dev.us.auth0.com/`],
+  algorithms: ['RS256']
+});
+
 // Retrieve all employees
-router.get('/', cityController.findAll);
+router.get('/', checkJwt, cityController.findAll);
 
 // Create a new employee
-router.post('/', cityController.create);
+router.post('/', checkJwt,cityController.create);
 
 // Retrieve a single employee with id
-router.get('/:id', cityController.findById);
+router.get('/:id', checkJwt,cityController.findById);
 
 // Update a employee with id
-router.put('/:id', cityController.update);
+router.put('/:id', checkJwt,cityController.update);
 
 // Delete a employee with id
-router.delete('/:id', cityController.delete);
+router.delete('/:id', checkJwt,cityController.delete);
 
 module.exports = router
